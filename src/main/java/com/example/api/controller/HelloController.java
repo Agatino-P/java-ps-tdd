6e60abb9bf8a006d19651c8b561b994f337f4186
User <user@example.com>
2026-03-13 12:57:01 +0000

    added the first test

---
+++ src/main/java/com/example/api/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping()
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("get-string")
+    public String getMethodName(@RequestParam String inputString) {
+        return inputString;
+    }
+    
+}
---
+++ src/test/java/com/example/api/controller/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("experimenting with mockmvc")
+    void testHello() throws Exception {
+        mockMvc.perform(get("/api/hello/get-string?inputString=hello123"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello123"));
+    }
+
+    @Test
+    @DisplayName("Test /api/hello endpoint")
+    void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Spring Boot!"));
+    }
+
+}
